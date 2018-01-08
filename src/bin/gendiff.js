5695833775625e48079d9ef2a5f7b55db7f8302c
VladislavHexlet <vladislavprh1@gmail.com>
2018-01-08 16:17:15 +0300

    Display help

---
+++ src/bin/gendiff.js
@@ -0,0 +1,9 @@
+#!/usr/bin/env node
+import program from 'commander';
+
+program
+  .arguments('<firstConfig> <secondConfig>')
+  .description('Compares two configuration files and shows a difference.')
+  .option('-V, --version', 'output the version number')
+  .option('-f, --format [type]', 'Output format')
+  .parse(process.argv);
