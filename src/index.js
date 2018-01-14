e268cf0eb60903d37043dccb3633edbc751a6146
VladislavHexlet <vladislavprh1@gmail.com>
2018-01-14 10:00:40 +0300

    Recursive comparison

--- src/index.js
+++ src/index.js
@@ -4,44 +4,88 @@ import yaml from 'js-yaml';
 import ini from 'ini';
 import path from 'path';
 
+
 const parser = {
   '.json': JSON.parse,
   '.yaml': yaml.safeLoad,
   '.ini': ini.parse,
 };
 
-
-const compare = (oldConfigObj, newConfigObj, repeatN = 1) => {
-  const oldConfigObjKeys = Object.keys(oldConfigObj);
-  const newConfigObjKeys = Object.keys(newConfigObj);
-  const unitedKeys = _.union(oldConfigObjKeys, newConfigObjKeys);
-  const indentLength = '  '.repeat(repeatN);
-  const resultArr = unitedKeys.map((el) => {
-    // console.log(newConfigObj[el]);
-    const newEl = newConfigObj[el];
-    const oldEl = oldConfigObj[el];
-    if (newEl === oldEl) {
-      if (newEl instanceof Object) {
-        return `${indentLength}  ${el}: ${JSON.stringify(newEl, null, '...')}\n`;
+const parseToAst = (oldFile, newFile) => {
+  const oldFileKeys = Object.keys(oldFile);
+  const newFileKeys = Object.keys(newFile);
+  const unitedKeys = _.union(oldFileKeys, newFileKeys);
+  return unitedKeys.map((key) => {
+    if (newFile[key] === oldFile[key]) {
+      if (!(newFile[key] instanceof Object)) {
+        return {
+          name: key, type: 'stay', value: newFile[key],
+        };
       }
-      return `${indentLength}  ${el}: ${newEl}\n`;
-    } else if (!newConfigObjKeys.includes(el)) {
-      if (oldEl instanceof Object) {
-        return `${indentLength}- ${el}: ${JSON.stringify(oldEl, null, '...')}\n`;
+      return {
+        name: key, type: 'stay', value: '', children: parseToAst(oldFile[key], newFile[key]),
+      };
+    } else if (!newFileKeys.includes(key)) {
+      if (!(oldFile[key] instanceof Object)) {
+        return {
+          name: key, type: 'removed', value: oldFile[key],
+        };
       }
-      return `${indentLength}- ${el}: ${oldEl}\n`;
-    } else if (!oldConfigObjKeys.includes(el)) {
-      if (newEl instanceof Object) {
-        return `${indentLength}+ ${el}: ${JSON.stringify(newEl, null, '...')}\n`;
+      return {
+        name: key, type: 'removed', value: '', children: parseToAst(oldFile[key], oldFile[key]),
+      };
+    } else if (!oldFileKeys.includes(key)) {
+      if (!(newFile[key] instanceof Object)) {
+        return {
+          name: key, type: 'added', value: newFile[key],
+        };
       }
-      return `${indentLength}+ ${el}: ${newEl}\n`;
+      return {
+        name: key, type: 'added', value: '', children: parseToAst(newFile[key], newFile[key]),
+      };
     }
-    if (newEl instanceof Object) {
-      return `${indentLength}  ${el}: {\n ${compare(oldEl, newEl, repeatN + 2)}  ${indentLength}}\n`;
+    if (newFile[key] instanceof Object) {
+      return {
+        name: key, type: 'stay', value: '', children: parseToAst(oldFile[key], newFile[key]),
+      };
     }
-    return [`${indentLength}+ ${el}: ${newEl}`, `\n${indentLength}- ${el}: ${oldEl}\n`];
+    return {
+      name: key, type: 'updated', previousValue: oldFile[key], newValue: newFile[key],
+    };
   });
-  return resultArr;
+};
+
+const createRootAst = (oldFile, newFile) => ({ children: parseToAst(oldFile, newFile) });
+
+const render = (ast, repeatN = 1) => {
+  // const content = ast.children ? ast.children.map(render).join('') : ast.value;
+  const indentLength = '    '.repeat(repeatN);
+  const indentLengthSigns = indentLength.slice(0, indentLength.length - 2);
+  const result = ast.children.map((node) => {
+    switch (node.type) {
+      case 'stay':
+        if (node.children) {
+          return `${indentLength}${node.name}: {\n${render(node, repeatN + 1)}\n${indentLength}}`;
+        }
+        return `${indentLength}${node.name}: ${node.value}`;
+        // return `${ast.name}: ${content}\n`;
+      case 'added':
+        if (node.children) {
+          return `${indentLengthSigns}+ ${node.name}: {\n${render(node, repeatN + 1)}\n${indentLength}}`;
+        }
+        return `${indentLengthSigns}+ ${node.name}: ${node.value}`;
+      case 'removed':
+        if (node.children) {
+          return `${indentLengthSigns}- ${node.name}: {\n${render(node, repeatN + 1)}\n${indentLength}}`;
+        }
+        return `${indentLengthSigns}- ${node.name}: ${node.value}`;
+      case 'updated':
+        return `${indentLengthSigns}+ ${node.name}: ${node.newValue}\n${indentLengthSigns}- ${node.name}: ${node.previousValue}`;
+      default:
+        return 1;
+    }
+  });
+  return result.join('\n');
 };
 
 const genDiff = (pathOldConfig, pathNewConfig) => {
@@ -51,10 +95,9 @@ const genDiff = (pathOldConfig, pathNewConfig) => {
   const parse = parser[fileExt];
   const oldConfigObj = parse(oldFile);
   const newConfigObj = parse(newFile);
-  const diff = compare(oldConfigObj, newConfigObj);
-
-  const result = _.flattenDeep(diff);
-  return `\n{\n ${diff.join('')}\n}\n`;
+  const ast = createRootAst(oldConfigObj, newConfigObj);
+  const diff = render(ast);
+  return `{\n${diff}\n}\n`;
 };
 
 export default genDiff;
